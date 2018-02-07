c06890fab9e36de7be234a0f5d89e1b48dd14c2b
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-02-07 14:35:13 +0630

    Adding another file

---
+++ src/com/bizleap/Assignment_Six.java
@@ -0,0 +1,30 @@
+package com.bizleap;
+
+/**
+ * Created by M2h_Laptop on 2/7/2018.
+ */
+public class Assignment_Six {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 3, 4, 3, 5, 6, 4, 7, 8, 7, 8, 8};
+
+        for(int i=0; i<arr.length;i++){
+            for(int j=i+1; j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    System.out.println("Number: "+arr[i]+" found repeating at position: "+i+" , repeated at position "+j);
+                }
+            }
+        }
+        
+    }
+    public int getNonDuplicateInt(){
+        int[] list = {0, 1, 2, 3, 4, 3, 5, 6, 4, 7, 8, 7, 8, 8};
+        for(int i=0; i<list.length;i++){
+            for(int j=i+1; j<list.length;j++){
+                if(list[i] == list[j]){
+
+                }
+            }
+        }
+        return 1;
+    }
+}
